913325eb29bd4d8e2d4ebc290279b08c54bbecc5
zhangliang <zhangliang@ypi.edu.cn>
2014-04-07 16:14:03 +0800

    add baidu map

--- LocationMonitor/src/com/flashzhang/locationmonitor/MainActivity.java
+++ LocationMonitor/src/com/flashzhang/locationmonitor/MainActivity.java
@@ -3,7 +3,11 @@ package com.flashzhang.locationmonitor;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
@@ -17,6 +21,18 @@ public class MainActivity extends Activity
 		TextView serialno=(TextView)findViewById(R.id.serialno);
 		String m_szAndroidID = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
 		serialno.setText(m_szAndroidID);
+		Button button1=(Button)findViewById(R.id.button1);
+		button1.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				Intent myintent=new Intent(MainActivity.this, MapActivity.class);
+				startActivity(myintent);
+			}
+		});
 	}
 
 	@Override
